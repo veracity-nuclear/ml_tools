Fixed up order of batch normalization and dropout layers

ml_tools/model/nn_strategy/compound_layer.py
@@ -8,7 +8,6 @@ import numpy as np
 # Pylint appears to not be handling the tensorflow imports correctly
 # pylint: disable=import-error, no-name-in-module, no-member
 import tensorflow as tf
-from tensorflow.keras import KerasTensor
 
 from ml_tools.model.nn_strategy.layer import Layer, gather_indices
 
@@ -105,7 +104,7 @@ class CompoundLayer(Layer):
                     self.batch_normalize,
                     self.layer_normalize)
 
-    def _build(self, input_tensor: KerasTensor) -> KerasTensor:
+    def build(self, input_tensor: tf.Tensor) -> tf.Tensor:
         assert all(index < input_tensor.shape[2] for spec in self.input_specifications for index in spec), \
             "input specification index greater than input feature vector length"
         split_inputs = [tf.keras.layers.TimeDistributed(
@@ -114,6 +113,16 @@ class CompoundLayer(Layer):
 
         outputs = [layer.build(split) for layer, split in zip(self._layers, split_inputs)]
         x = tf.keras.layers.Concatenate(axis=-1)(outputs)
+
+        if self.batch_normalize:
+            x = tf.keras.layers.TimeDistributed(tf.keras.layers.BatchNormalization())(x)
+
+        if self.layer_normalize:
+            x = tf.keras.layers.TimeDistributed(tf.keras.layers.LayerNormalization())(x)
+
+        if self.dropout_rate > 0.:
+            x = tf.keras.layers.TimeDistributed(tf.keras.layers.Dropout(rate=self.dropout_rate))(x)
+
         return x
 
 

ml_tools/model/nn_strategy/dense.py
@@ -6,7 +6,6 @@ import h5py
 # Pylint appears to not be handling the tensorflow imports correctly
 # pylint: disable=import-error, no-name-in-module, no-member
 import tensorflow as tf
-from tensorflow.keras import KerasTensor
 
 from ml_tools.model.nn_strategy.layer import Layer, Activation
 
@@ -81,8 +80,20 @@ class Dense(Layer):
                           self.layer_normalize
                    )
 
-    def _build(self, input_tensor: KerasTensor) -> KerasTensor:
-        x = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(units=self.units, activation=self.activation))(input_tensor)
+    def build(self, input_tensor: tf.Tensor) -> tf.Tensor:
+        x = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(units=self.units, activation=None))(input_tensor)
+
+        if self.batch_normalize:
+            x = tf.keras.layers.TimeDistributed(tf.keras.layers.BatchNormalization())(x)
+
+        if self.layer_normalize:
+            x = tf.keras.layers.TimeDistributed(tf.keras.layers.LayerNormalization())(x)
+
+        x = tf.keras.layers.Activation(self.activation)(x)
+
+        if self.dropout_rate > 0.:
+            x = tf.keras.layers.TimeDistributed(tf.keras.layers.Dropout(rate=self.dropout_rate))(x)
+
         return x
 
     def save(self, group: h5py.Group) -> None:

ml_tools/model/nn_strategy/layer.py
@@ -6,7 +6,6 @@ import h5py
 # Pylint appears to not be handling the tensorflow imports correctly
 # pylint: disable=import-error, no-name-in-module
 import tensorflow as tf
-from tensorflow.keras import KerasTensor
 from tensorflow.keras.saving import register_keras_serializable
 
 
@@ -149,34 +148,13 @@ class Layer(ABC):
         Hash generation is consistent with the 1e-9 float comparison equality relative tolerance
         """
 
-    def build(self, input_tensor: KerasTensor) -> KerasTensor:
-        """ Method for constructing the layer
-
-        Parameters
-        ----------
-        input_tensor : KerasTensor
-            The input tensor for the layer
-        """
-        x = self._build(input_tensor)
-
-        if self.batch_normalize:
-            x = tf.keras.layers.TimeDistributed(tf.keras.layers.BatchNormalization())(x)
-
-        if self.layer_normalize:
-            x = tf.keras.layers.TimeDistributed(tf.keras.layers.LayerNormalization())(x)
-
-        if self.dropout_rate > 0.:
-            x = tf.keras.layers.TimeDistributed(tf.keras.layers.Dropout(rate=self.dropout_rate))(x)
-
-        return x
-
     @abstractmethod
-    def _build(self, input_tensor: KerasTensor) -> KerasTensor:
+    def build(self, input_tensor: tf.Tensor) -> tf.Tensor:
         """ Method for constructing the layer without any dropout or normalization
 
         Parameters
         ----------
-        input_tensor : KerasTensor
+        input_tensor : tf.Tensor
             The input tensor for the layer
         """
 

ml_tools/model/nn_strategy/lstm.py
@@ -7,7 +7,6 @@ import h5py
 # Pylint appears to not be handling the tensorflow imports correctly
 # pylint: disable=import-error, no-name-in-module, no-member
 import tensorflow as tf
-from tensorflow.keras import KerasTensor
 
 from ml_tools.model.nn_strategy.layer import Layer, Activation
 
@@ -116,12 +115,22 @@ class LSTM(Layer):
                           self.layer_normalize
                    )
 
-    def _build(self, input_tensor: KerasTensor) -> KerasTensor:
+    def build(self, input_tensor: tf.Tensor) -> tf.Tensor:
         x = tf.keras.layers.LSTM(units                = self.units,
                                  activation           = self.activation,
                                  return_sequences     = True,
                                  recurrent_activation = self.recurrent_activation,
                                  recurrent_dropout    = self.recurrent_dropout_rate)(input_tensor)
+
+        if self.batch_normalize:
+            x = tf.keras.layers.BatchNormalization()(x)
+
+        if self.layer_normalize:
+            x = tf.keras.layers.LayerNormalization()(x)
+
+        if self.dropout_rate > 0.:
+            x = tf.keras.layers.Dropout(rate=self.dropout_rate)(x)
+
         return x
 
     def save(self, group: h5py.Group) -> None:

ml_tools/model/nn_strategy/pass_through.py
@@ -5,8 +5,8 @@ from decimal import Decimal
 import h5py
 
 # Pylint appears to not be handling the tensorflow imports correctly
-# pylint: disable=import-error, no-name-in-module
-from tensorflow.keras import KerasTensor
+# pylint: disable=import-error, no-name-in-module, no-member
+import tensorflow as tf
 
 from ml_tools.model.nn_strategy.layer import Layer
 
@@ -40,8 +40,20 @@ class PassThrough(Layer):
                     self.batch_normalize,
                     self.layer_normalize)
 
-    def _build(self, input_tensor: KerasTensor) -> KerasTensor:
-        return input_tensor
+    def build(self, input_tensor: tf.Tensor) -> tf.Tensor:
+
+        x = input_tensor
+
+        if self.batch_normalize:
+            x = tf.keras.layers.TimeDistributed(tf.keras.layers.BatchNormalization())(x)
+
+        if self.layer_normalize:
+            x = tf.keras.layers.TimeDistributed(tf.keras.layers.LayerNormalization())(x)
+
+        if self.dropout_rate > 0.:
+            x = tf.keras.layers.TimeDistributed(tf.keras.layers.Dropout(rate=self.dropout_rate))(x)
+
+        return x
 
     def save(self, group: h5py.Group) -> None:
         group.create_dataset('type'        ,     data='PassThrough', dtype=h5py.string_dtype())

ml_tools/model/nn_strategy/spatial_conv.py
@@ -7,7 +7,6 @@ import h5py
 # Pylint appears to not be handling the tensorflow imports correctly
 # pylint: disable=import-error, no-name-in-module, no-member
 import tensorflow as tf
-from tensorflow.keras import KerasTensor
 
 from ml_tools.model.nn_strategy.layer import Layer, Activation
 
@@ -188,7 +187,7 @@ class SpatialConv(Layer):
                           self.layer_normalize
                    )
 
-    def _build(self, input_tensor: KerasTensor) -> KerasTensor:
+    def build(self, input_tensor: tf.Tensor) -> tf.Tensor:
         assert input_tensor.shape[-1] % (self.input_shape[0] * self.input_shape[1] * self.input_shape[2]) == 0, \
             "Input tensor shape is not divisible by the expected input 3D shape"
 
@@ -200,8 +199,21 @@ class SpatialConv(Layer):
                                                                    kernel_size = self.kernel_size,
                                                                    strides     = self.strides,
                                                                    padding     = 'same' if self.padding else 'valid',
-                                                                   activation  = self.activation))(x)
+                                                                   activation  = None))(x)
+
+        if self.batch_normalize:
+            x = tf.keras.layers.TimeDistributed(tf.keras.layers.BatchNormalization())(x)
+
+        if self.layer_normalize:
+            x = tf.keras.layers.TimeDistributed(tf.keras.layers.LayerNormalization())(x)
+
+        x = tf.keras.layers.Activation(self.activation)(x)
+
+        if self.dropout_rate > 0.:
+            x = tf.keras.layers.TimeDistributed(tf.keras.layers.SpatialDropout3D(rate=self.dropout_rate))(x)
+
         x = tf.keras.layers.TimeDistributed(tf.keras.layers.Flatten())(x)
+
         return x
 
     def save(self, group: h5py.Group) -> None:
@@ -344,7 +356,7 @@ class SpatialMaxPool(Layer):
                           self.layer_normalize
                    )
 
-    def _build(self, input_tensor: KerasTensor) -> KerasTensor:
+    def build(self, input_tensor: tf.Tensor) -> tf.Tensor:
         assert input_tensor.shape[-1] % (self.input_shape[0] * self.input_shape[1] * self.input_shape[2]) == 0, \
             "Input tensor shape is not divisible by the expected input 3D shape"
 
@@ -355,7 +367,18 @@ class SpatialMaxPool(Layer):
         x = tf.keras.layers.TimeDistributed(tf.keras.layers.MaxPooling3D(pool_size = self.pool_size,
                                                                          strides   = self.strides,
                                                                          padding   = 'same' if self.padding else 'valid'))(x)
+
+        if self.batch_normalize:
+            x = tf.keras.layers.TimeDistributed(tf.keras.layers.BatchNormalization())(x)
+
+        if self.layer_normalize:
+            x = tf.keras.layers.TimeDistributed(tf.keras.layers.LayerNormalization())(x)
+
+        if self.dropout_rate > 0.:
+            x = tf.keras.layers.TimeDistributed(tf.keras.layers.SpatialDropout3D(rate=self.dropout_rate))(x)
+
         x = tf.keras.layers.TimeDistributed(tf.keras.layers.Flatten())(x)
+
         return x
 
     def save(self, group: h5py.Group) -> None:

ml_tools/model/nn_strategy/transformer.py
@@ -7,7 +7,6 @@ import h5py
 # Pylint appears to not be handling the tensorflow imports correctly
 # pylint: disable=import-error, no-name-in-module, no-member
 import tensorflow as tf
-from tensorflow.keras import KerasTensor
 
 from ml_tools.model.nn_strategy.layer import Layer, Activation
 
@@ -28,10 +27,6 @@ class Transformer(Layer):
         Activation function to use for the Feed Forward Network of the Transformer
     dropout_rate : float, optional
         Dropout rate for the layer. Default is 0.0 (no dropout).
-    batch_normalize : bool
-        Whether or not batch normalization will be performed on the layer output prior to dropout
-    layer_normalize : bool
-        Whether or not layer normalization will be performed on the layer output prior to dropout
 
     Attributes
     ----------
@@ -86,10 +81,8 @@ class Transformer(Layer):
                  model_dim:        int,
                  ff_dim:           int,
                  activation:       Activation = 'relu',
-                 dropout_rate:     float = 0.,
-                 batch_normalize:  bool = False,
-                 layer_normalize:  bool = False):
-        super().__init__(dropout_rate, batch_normalize, layer_normalize)
+                 dropout_rate:     float = 0.,):
+        super().__init__(dropout_rate, batch_normalize=False, layer_normalize=False)
         self.num_heads        = num_heads
         self.model_dim        = model_dim
         self.ff_dim           = ff_dim
@@ -102,9 +95,7 @@ class Transformer(Layer):
                   self.model_dim        == other.model_dim and
                   self.ff_dim           == other.ff_dim and
                   self.activation       == other.activation and
-                  isclose(self.dropout_rate, other.dropout_rate, rel_tol=1e-9) and
-                  self.batch_normalize  == other.batch_normalize and
-                  self.layer_normalize  == other.layer_normalize)
+                  isclose(self.dropout_rate, other.dropout_rate, rel_tol=1e-9))
         )
 
     def __hash__(self) -> int:
@@ -112,27 +103,37 @@ class Transformer(Layer):
                           self.model_dim,
                           self.ff_dim,
                           Decimal(self.dropout_rate).quantize(Decimal('1e-9'))),
-                          self.batch_normalize,
-                          self.layer_normalize,
                           self.activation
                    )
 
-    def _build(self, input_tensor: KerasTensor) -> KerasTensor:
-        # Project input_tensor to model dimensions if they are not the same
-        input_tensor = tf.keras.layers.Dense(self.model_dim)(input_tensor) \
-                       if input_tensor.shape[-1] != self.model_dim else input_tensor
+    def build(self, input_tensor: tf.Tensor) -> tf.Tensor:
+        # Project input to model_dim if needed
+        if input_tensor.shape[-1] != self.model_dim:
+            input_tensor = tf.keras.layers.Dense(self.model_dim)(input_tensor)
 
+        # Multi-head self-attention
         attention = tf.keras.layers.MultiHeadAttention(num_heads = self.num_heads,
                                                        key_dim   = self.model_dim)(input_tensor, input_tensor)
-        attention = tf.keras.layers.Dropout(rate=self.dropout_rate)(attention) if self.dropout_rate > 0. else attention
-        attention = tf.keras.layers.LayerNormalization(epsilon=1e-6)(attention + input_tensor)
+        if self.dropout_rate > 0.:
+            attention = tf.keras.layers.Dropout(self.dropout_rate)(attention)
 
-        feedfoward = tf.keras.layers.Dense(self.ff_dim, activation=self.activation)(attention)
-        feedfoward = tf.keras.layers.Dense(self.model_dim)(feedfoward)
-        feedfoward = tf.keras.layers.Dropout(rate=self.dropout_rate)(feedfoward) if self.dropout_rate > 0. else feedfoward
+        attention = tf.keras.layers.LayerNormalization(epsilon=1e-6)(input_tensor + attention)
 
-        return feedfoward + attention
+        # Feedforward network (MLP block)
+        feedforward = tf.keras.layers.Dense(self.ff_dim, activation=self.activation)(attention)
 
+        if self.dropout_rate > 0.:
+            feedforward = tf.keras.layers.Dropout(self.dropout_rate)(feedforward)
+
+        feedforward = tf.keras.layers.Dense(self.model_dim)(feedforward)
+
+        if self.dropout_rate > 0.:
+            feedforward = tf.keras.layers.Dropout(self.dropout_rate)(feedforward)
+
+        # Residual connection + LayerNorm (second norm)
+        x = tf.keras.layers.LayerNormalization(epsilon=1e-6)(attention + feedforward)
+
+        return x
 
     def save(self, group: h5py.Group) -> None:
         group.create_dataset('type',                    data='Transformer', dtype=h5py.string_dtype())
@@ -141,8 +142,6 @@ class Transformer(Layer):
         group.create_dataset('feed_forward_dimensions', data=self.ff_dim)
         group.create_dataset('activation_function',     data=self.activation, dtype=h5py.string_dtype())
         group.create_dataset('dropout_rate',            data=self.dropout_rate)
-        group.create_dataset('batch_normalize',         data=self.batch_normalize)
-        group.create_dataset('layer_normalize',         data=self.layer_normalize)
 
 
     @classmethod
@@ -151,6 +150,4 @@ class Transformer(Layer):
                    model_dim        =   int(group["model_dimensions"       ][()]),
                    ff_dim           =   int(group["feed_forward_dimensions"][()]),
                    activation       =       group["activation_function"    ][()].decode('utf-8'),
-                   dropout_rate     = float(group["dropout_rate"           ][()]),
-                   batch_normalize  =  bool(group["batch_normalize"        ][()]),
-                   layer_normalize  =  bool(group["layer_normalize"        ][()]))
+                   dropout_rate     = float(group["dropout_rate"           ][()]))